4522a23a260f1ab2465266ff448f1ca56a72252c
Silvio <silviosisa@gmail.com>
2023-01-05 17:47:47 -0300

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,7 @@ async function run() {
   const s3Uri = `s3://${bucket}/`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${buketRegion}`);
 
-  core.notice("Hello from my custom Javascript Action");
-  core.info("Deploying to S3");
+  const websiteUrl = `http://${bucket}.s3-website-${buketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 run();
